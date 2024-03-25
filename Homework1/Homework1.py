9724bdef28cb144122a372f0e66a877f3eeb518b
User <User@DESKTOP-8KUQDRH>
2024-03-25 11:17:59 +0500

    Добавьте файлы проекта.

---
+++ Homework1/Homework1.py
@@ -0,0 +1,7 @@
+# Получаем данные от пользователя
+pet_type = input('1: ')
+pet_name = input('2: ')
+pet_age = input('3: ')
+
+# Создаем и выводим предложение
+print ('1 ', {pet_type}, '2 ', {pet_name}, '.', '3: ', {pet_age}, '.')
